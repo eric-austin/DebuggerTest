698555aa0abbccc8010ac115b60dfcb28804e6f6
Eric Austin <31332860+eric-austin@users.noreply.github.com>
2020-02-10 13:14:28 -0700

    Update LotsOfVars.java

--- src/LotsOfVars.java
+++ src/LotsOfVars.java
@@ -3,7 +3,7 @@
 public class LotsOfVars {
 	//instance variables
 	private int a, b, c;
-	private int[] as, bs;;
+	private int[] as, bs;
 	private Boolean isSmall, isGreen, isDog;
 	private String msg, output, input, err;
 
